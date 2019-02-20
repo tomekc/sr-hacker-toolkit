e20c4152a176f92a6401baf2c728e55b1872c1be
Tomek Cejner <tomek@smartrecruiters.com>
2019-02-20 14:40:15 +0100

    Heroku compatibility changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 const pjson = require('./package.json');
 
 // ========================
